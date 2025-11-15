ba5bed8d080a2cacdc92641365a18cafa11ce847
SharjeelNadir <sharjeelnadir127@gmail.com>
2025-11-15 19:06:51 +0500

    Fix flake8 spacing issues for Task 2 on CI branch

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World! This is Flask CI/CD with Kubernetes!"
-
+    return "Hello, World!"
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
